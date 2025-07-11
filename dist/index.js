90c9622083585b5b3f5aa0afcb330326d3bf32b4
밍 <sucresucces@icloud.com>
2025-07-11 18:05:08 +0900

    타입스크립트 컴파일러 옵션 설정하기
    {
      "compilerOptions": {
        "target": "esnext",
        "module": "esnext",
        "outDir": "dist",
        "strict": false,
        "moduleDetection": "force",
        "skipLibCheck": true
      },
      "include": ["src"]
    }
    
    1. 기본 구조 이해하기
    - complierOptions : 타입스크립트가 코드를 어떻게 컴파일할지 결정하는 핵심 설정들
    - include : 어떤 폴더나 파일을 컴파일 대상으로 포함할지 지정(src 폴더 전체 포함)
    
    2. 주요 옵션 해설
    
    | 옵션 이름             | 의미                  | 효과                                        |
    | ----------------- | ------------------- | ----------------------------------------- |
    | `target`          | 어떤 버전의 자바스크립트로 변환할지 | `esnext`: 최신 JS 문법 유지                     |
    | `module`          | 모듈 시스템 설정           | `esnext`: 최신 모듈 시스템 사용 (import/export 유지) |
    | `outDir`          | 컴파일된 JS 파일이 저장될 경로  | `dist` 폴더로 출력됨                            |
    | `strict`          | 타입 검사 얼마나 엄격하게 할지   | `false`면 느슨하게 검사 (엄격 모드 꺼짐)               |
    | `skipLibCheck`    | 외부 라이브러리의 타입 검사 생략  | 빠른 빌드를 위해 검사 생략                           |
    | `moduleDetection` | 모듈로 인식하는 기준 설정      | `force`: 모든 파일을 강제로 모듈로 처리 (전역 스코프 방지)    |
    
    3. 전역 모듈 문제 이해하기
    기본 동작
    - 타입스크립트는 파일 안에 import나 export가 없으면 그 파일을 전역 모듈로 취급함.
    -> 이러면 전역 변수/ 함수처럼 충돌 위험이 생김
    
    전역 모듈의 단점
    - 서로 다른 파일이 같은 이름을 사용할 경우 충돌 발생
    - 격리된 모듈 스코프가 아님
    
    4. 해결 방법 요약
    - export{}
        - 아무 것도 내보내지 않아도, 파일을 모듈로 간주함(격리됨)
    - "moduleDetection" : "force"
       - 모든 파일을 강제로 모듈로 취급(import/export 없어도 모듈화 됨)

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+const a = 1;
+export {};
+// 자동으로 추가됨 export {}
+// export{} : 독립된 격리된 모듈
+// "moduleDetection": "force" : 모듈로 인식하게 함
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-console.log("Hello TypeScript");
-const a: number = 1;
+const a = 1;
+
+// 1. 타입스크립트는 기본적으로 전역 모듈(global module)로 인식
+// 2. 개별 export import가 없으면 전역 모듈로 인식
+// 3. moduleDetection 옵션을 사용하면 모듈로 인식하게 할 수 있음
+
+// 해결방법1
+// export {};
+// // export{} : 독립된 격리된 모듈
+// 해결방법2
+// "moduleDetection": "force"
